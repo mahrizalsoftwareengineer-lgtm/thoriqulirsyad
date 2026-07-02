60530ae1424a22b00cb24fd253dc77f9790da941
Mahrizal IT <mahrizalit@gmail.com>
2026-07-02 22:49:18 +0700

    chore: add security headers and fix npm vulnerabilities

--- app/layout.tsx
+++ app/layout.tsx
@@ -151,9 +151,9 @@ export default function RootLayout({
 
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-GMYBE4KZYG"
-          strategy="afterInteractive"
+          strategy="lazyOnload"
         />
-        <Script id="google-analytics" strategy="afterInteractive">
+        <Script id="google-analytics" strategy="lazyOnload">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
